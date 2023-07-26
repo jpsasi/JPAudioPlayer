54fcfd2e28cce2d9971d23cc2069c35ebe388f0c
Sasikumar JP <jpsasi@gmail.com>
2023-07-26 21:16:56 +0530

    Interruption handling code added

--- Sources/JPAudioPlayer/JPAudioPlayer.swift
+++ Sources/JPAudioPlayer/JPAudioPlayer.swift
@@ -6,8 +6,9 @@
 
 import Foundation
 import AVFoundation
+import MediaPlayer
 
-enum JPAudioPlayerStatus {
+public enum JPAudioPlayerStatus {
   case notInitialized
   case buffering
   case playing
@@ -17,67 +18,147 @@ enum JPAudioPlayerStatus {
   case unknown
 }
 
-public class JPAudioPlayer: NSObject {
-  let player: AVPlayer
-  let sessionController: JPAudioSessionController
-  var playerStatus: JPAudioPlayerStatus = .notInitialized
+public class JPAudioPlayer: NSObject, ObservableObject {
+  var player: AVPlayer?
+  var sessionController: JPAudioSessionController?
+  let remoteCommandCenter: MPRemoteCommandCenter = MPRemoteCommandCenter.shared()
+  #if os(iOS)
+  var nowPlayingSession: MPNowPlayingSession?
+  #endif
+  @Published public var playerStatus: JPAudioPlayerStatus = .notInitialized
+  public var statusString: String {
+    switch playerStatus {
+      case .notInitialized:
+        return "Not Initialized"
+      case .buffering:
+        return "Buffering"
+      case .playing:
+        return "Playing"
+      case .stopped:
+        return "Stopped"
+      case .paused:
+        return "Paused"
+      case .failed:
+        return "Failed"
+      case .unknown:
+        return "Unknown"
+    }
+  }
+  @Published var title: String = ""
   private static var playerItemContext = 0
   
-  public init(url: URL) {
-    let urlAsset = AVURLAsset(url: url)
-    let playerItem = AVPlayerItem(asset: urlAsset)
-    player = AVPlayer(playerItem: playerItem)
-    sessionController = JPAudioSessionController()
-    super.init()
-    
-    playerItem.addObserver(self, forKeyPath: "status", options: .new, context: &JPAudioPlayer.playerItemContext)
-    
-    let metaData = AVPlayerItemMetadataOutput(identifiers: nil)
-    metaData.setDelegate(self, queue: .main)
-    playerItem.add(metaData)
+  public var isPlaying: Bool {
+    return playerStatus == .buffering || playerStatus == .playing
   }
   
   deinit {
   }
   
-  func play() {
+  public func startStreamAudio(url: URL) {
+    let urlAsset = AVURLAsset(url: url)
+    let playerItem = AVPlayerItem(asset: urlAsset)
+    player = AVPlayer(playerItem: playerItem)
+    sessionController = JPAudioSessionController()
+    sessionController?.sessionDelegate = self
+    setupAudioSession()
+    playerItem.addObserver(self, forKeyPath: "status", options: [.new], context: nil)
+    player?.addObserver(self, forKeyPath: "rate", options: [.new, .initial], context: nil)
+    
+    let metaData = AVPlayerItemMetadataOutput(identifiers: nil)
+    metaData.setDelegate(self, queue: .main)
+    playerItem.add(metaData)
+    playerStatus = .buffering
+    updateNowPlayingInfo()
+    activateRemoteControl()
+    self.nowPlayingSession = MPNowPlayingSession(players: [player!])
+    self.nowPlayingSession?.delegate = self
+  }
+  
+  private func setupAudioSession() {
 #if os(iOS)
-    sessionController.configure()
+    do {
+      print("configuring session control")
+      try sessionController?.configure()
+    } catch {
+      print("session controller configure exception")
+    }
 #endif
-    player.play()
   }
   
-  func pause() {
-    player.pause()
+  private func updateNowPlayingInfo(title: String = "") {
+    let nowPlayingInfo: [String:Any] = [
+      MPNowPlayingInfoPropertyIsLiveStream: true,
+      MPMediaItemPropertyAlbumTitle: "London Telugu Radio",
+      MPMediaItemPropertyTitle: title
+    ]
+    if let player, let playerItem = player.currentItem {
+      playerItem.nowPlayingInfo = nowPlayingInfo
+      
+    }
+    MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
   }
   
-  func stop() {
-    player.pause()
+  private func activateRemoteControl() {
+    remoteCommandCenter.playCommand.addTarget { _ in
+      self.play()
+      return .success
+    }
+    
+    remoteCommandCenter.stopCommand.addTarget { _ in
+      self.stop()
+      return .success
+    }
+    
+    remoteCommandCenter.pauseCommand.addTarget { _ in
+      self.pause()
+      return .success
+    }
+
   }
   
-  func resume() {
-    player.play()
+  public func play() {
+    player?.play()
+  }
+  
+  public func pause() {
+    player?.pause()
+    playerStatus = .paused
+  }
+  
+  public func stop() {
+    player?.pause()
+    playerStatus = .stopped
+  }
+  
+  public func resume() {
+    player?.play()
   }
   
   public override func observeValue(forKeyPath keyPath: String?,
                                     of object: Any?,
                                     change: [NSKeyValueChangeKey : Any]?,
                                     context: UnsafeMutableRawPointer?) {
-    if let context, context == &JPAudioPlayer.playerItemContext {
+    if let keyPath, keyPath == "status" {
       if let statusCode = (change?[NSKeyValueChangeKey.newKey] as? NSNumber)?.intValue,
          let playerStatus = AVPlayer.Status(rawValue: statusCode) {
         if playerStatus == .readyToPlay {
           self.playerStatus = .playing
-          player.play()
+          player?.play()
         } else if playerStatus == .failed {
           self.playerStatus = .failed
-        } else if playerStatus == .unknown {
-          self.playerStatus = .unknown
         } else {
-          print("Player status \(playerStatus)")
+          self.playerStatus = .unknown
         }
       }
-    } else {
+    } else if let keyPath, keyPath == "rate" {
+      if let rateValue = (change?[NSKeyValueChangeKey.newKey] as? NSNumber)?.intValue {
+        print("rate \(rateValue)")
+        if rateValue > 0 {
+          playerStatus = .playing
+        }
+      }
+    }
+    else {
       super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
     }
   }
@@ -92,10 +173,38 @@ extension JPAudioPlayer: AVPlayerItemMetadataOutputPushDelegate {
       for item in group.items {
         if item.value is String {
           if let title = item.value as? String {
-            print("meta Data \(title)")
+            print("meta data \(title)")
+            updateNowPlayingInfo(title: title)
           }
         }
       }
+    }
+  }
+}
+
+@available(iOS 16.0, *)
+extension JPAudioPlayer: MPNowPlayingSessionDelegate {
+
+  public func nowPlayingSessionDidChangeActive(_ nowPlayingSession: MPNowPlayingSession) {
+    print("DidChange Active")
+  }
+  
+  public func nowPlayingSessionDidChangeCanBecomeActive(_ nowPlayingSession: MPNowPlayingSession) {
+    print("Can Become Active")
+  }
+}
+
+extension JPAudioPlayer: JPSessionControllerDelegate {
+  
+  public func sessionControllerDidBeginInterruption() {
+    stop()
+  }
+  
+  public func sessionControllerDidEndInterruption(canResume: Bool) {
+    if canResume {
+      play()
+    } else {
+      stop()
     }
   }
 }
--- Sources/JPAudioPlayer/JPAudioSessionController.swift
+++ Sources/JPAudioPlayer/JPAudioSessionController.swift
@@ -7,14 +7,59 @@
 
 import Foundation
 import AVFoundation
+ 
+public protocol JPSessionControllerDelegate: AnyObject {
+  func sessionControllerDidBeginInterruption()
+  func sessionControllerDidEndInterruption(canResume: Bool)
+}
 
 public class JPAudioSessionController: NSObject {
+  public weak var sessionDelegate: JPSessionControllerDelegate?
+  
+  init(sessionDelegate: JPSessionControllerDelegate? = nil) {
+    self.sessionDelegate = sessionDelegate
+  }
   
   public func configure() throws {
 #if os(iOS)
       let session = AVAudioSession.sharedInstance()
-      session.setCategory(.playback)
-      session.setMode(.spokenAudio)
+      try session.setCategory(.playback)
+      try session.setMode(.spokenAudio)
+    setupAudioSessionObservers()
 #endif
+  }
+  
+  public func setupAudioSessionObservers() {
+#if os(iOS)
+    let session = AVAudioSession.sharedInstance()
+    let notificationCenter = NotificationCenter.default
+    
+    notificationCenter.addObserver(forName: AVAudioSession.interruptionNotification, object: session, queue: .main) { [weak self] notification in
+      print("interruption notification")
+      if let self, let userInfo = notification.userInfo, let type = userInfo[AVAudioSessionInterruptionTypeKey] as? NSNumber {
+        if let interruptionType = AVAudioSession.InterruptionType(rawValue: UInt(type.intValue)) {
+          switch interruptionType {
+            case .began:
+              self.sessionDelegate?.sessionControllerDidBeginInterruption()
+            case .ended:
+              if let canResume = userInfo[AVAudioSessionInterruptionOptionKey] as? NSNumber {
+                self.sessionDelegate?.sessionControllerDidEndInterruption(canResume: canResume.boolValue)
+              }
+            @unknown default:
+              fatalError()
+          }
+        }
+      }
+    }
+    
+    notificationCenter.addObserver(forName: AVAudioSession.routeChangeNotification, object: session, queue: .main) { notification in
+      print("route change notification")
+    }
+    
+    notificationCenter.addObserver(forName: AVAudioSession.mediaServicesWereLostNotification, object: session, queue: .main) { notification in
+      
+    }
+#endif
+
   }
 }
